5149807a5187652df63d1f8a4921a99f8d5df0b5
Jerald Sheets <questy@gmail.com>
2014-03-20 17:22:49 -0500

    reverted to older classloading method

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,5 @@
 # CoA Splunk Module
 class coa-splunk {
-  class { '::coa-splunk::config': } ->
-  class { '::coa-splunk::service': } ->
-  Class['coa-splunk']
+  include coa-splunk::config
+  include coa-splunk::service
 }
